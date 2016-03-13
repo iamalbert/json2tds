b1d8c669094ed6ff82ea6f8c2a190bb1189f78f7
Wen Li Zhuang <wlzhuang@nlg.csie.ntu.edu.tw>
2016-03-14 01:17:40 +0800

    format 2

--- JsonNode.h
+++ JsonNode.h
@@ -2,9 +2,9 @@
 #define JSON_NODE_H
 
 extern "C" {
-    #include <lua.h>
-    #include <lualib.h>
-    #include <lauxlib.h>
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
 }
 
 #define LS lua_State *L
@@ -19,15 +19,15 @@ extern "C" {
 #include <cassert>
 
 struct JsonValue {
-	JsonValue * next;
-	JsonValue * member;
-	JsonValue();
+    JsonValue *next;
+    JsonValue *member;
+    JsonValue();
 
     virtual ~JsonValue();
 
-	virtual void breakLinks();
-	virtual std::ostream & print( std::ostream & os ) const;
-	virtual std::ostream & printJson( std::ostream & os ) const;
+    virtual void breakLinks();
+    virtual std::ostream &print(std::ostream &os) const;
+    virtual std::ostream &printJson(std::ostream &os) const;
     virtual int toLuaObject(LS);
     int asLuaObject(LS);
 };
@@ -35,87 +35,82 @@ struct JsonValue {
 struct JsonString;
 
 struct JsonPair : public JsonValue {
-	std::string* key;
-	JsonPair(std::string* k);
+    std::string *key;
+    JsonPair(std::string *k);
 
-	virtual ~JsonPair();
-	virtual std::ostream & print( std::ostream & os ) const;
-	virtual std::ostream & printJson( std::ostream & os ) const;
+    virtual ~JsonPair();
+    virtual std::ostream &print(std::ostream &os) const;
+    virtual std::ostream &printJson(std::ostream &os) const;
 
     virtual int toLuaObject(LS);
 };
 
-
 struct JsonObject : public JsonValue {
-	virtual std::ostream & print( std::ostream & os ) const;
-	virtual std::ostream & printJson( std::ostream & os ) const;
+    virtual std::ostream &print(std::ostream &os) const;
+    virtual std::ostream &printJson(std::ostream &os) const;
     virtual int toLuaObject(LS);
 };
 
 struct JsonArray : public JsonValue {
-	virtual std::ostream & print( std::ostream & os ) const ;
-	virtual std::ostream & printJson( std::ostream & os ) const;
+    virtual std::ostream &print(std::ostream &os) const;
+    virtual std::ostream &printJson(std::ostream &os) const;
     virtual int toLuaObject(LS);
 };
 
 struct JsonString : public JsonValue {
     std::string *value;
-	JsonString( std::string *s);
-	~JsonString();
+    JsonString(std::string *s);
+    ~JsonString();
 
-	virtual std::ostream & print( std::ostream & os ) const ;
-	virtual std::ostream & printJson( std::ostream & os ) const;
+    virtual std::ostream &print(std::ostream &os) const;
+    virtual std::ostream &printJson(std::ostream &os) const;
     virtual int toLuaObject(LS);
 };
 struct JsonNumber : public JsonValue {
     double value;
     JsonNumber(double v);
-	virtual std::ostream & print( std::ostream & os ) const ;
-	virtual std::ostream & printJson( std::ostream & os ) const;
+    virtual std::ostream &print(std::ostream &os) const;
+    virtual std::ostream &printJson(std::ostream &os) const;
     virtual int toLuaObject(LS);
 };
 struct JsonBoolean : public JsonValue {
     bool value;
-	JsonBoolean(bool b);
-	virtual std::ostream & print( std::ostream & os ) const ;
-	virtual std::ostream & printJson( std::ostream & os ) const;
+    JsonBoolean(bool b);
+    virtual std::ostream &print(std::ostream &os) const;
+    virtual std::ostream &printJson(std::ostream &os) const;
     virtual int toLuaObject(LS);
 };
 struct JsonNull : public JsonValue {
-	virtual std::ostream & print( std::ostream & os ) const ;
-	virtual std::ostream & printJson( std::ostream & os ) const;
+    virtual std::ostream &print(std::ostream &os) const;
+    virtual std::ostream &printJson(std::ostream &os) const;
     virtual int toLuaObject(LS);
 };
 
-
 struct JsonState {
-	JsonValue * value;
-	std::deque<JsonValue*> objList;
+    JsonValue *value;
+    std::deque<JsonValue *> objList;
 
-	JsonState();
+    JsonState();
 
-	template<class T, class ...Args>
-	T* newObject( Args&& ... args ){
-		static_assert( std::is_base_of< JsonValue, T >::value, 
-				"must derived from JsonValue");
-		T* obj = new T( std::forward<Args>(args)... );
-		objList.push_back(obj);
-		return obj;
-	}
+    template <class T, class... Args> T *newObject(Args &&... args) {
+        static_assert(std::is_base_of<JsonValue, T>::value,
+                      "must derived from JsonValue");
+        T *obj = new T(std::forward<Args>(args)...);
+        objList.push_back(obj);
+        return obj;
+    }
 
-	JsonValue *getJsonValue() const ;
+    JsonValue *getJsonValue() const;
 
-	void free();
-
+    void free();
 };
-
 
 struct JsonException : public std::runtime_error {
-	template<class ...Args>
-	JsonException(Args&& ... args ) :
-		std::runtime_error( std::forward<Args>(args)... ) {}
+    template <class... Args>
+    JsonException(Args &&... args)
+        : std::runtime_error(std::forward<Args>(args)...) {}
 };
 
-JsonValue * parse_json( FILE * );
+JsonValue *parse_json(FILE *);
 
 #endif
