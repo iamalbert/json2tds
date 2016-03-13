83d7ed2916f4a1bc2e848744feaf1c009c11b3d5
Wen Li Zhuang <wlzhuang@nlg.csie.ntu.edu.tw>
2016-03-14 01:14:24 +0800

    format

--- JsonNode.cpp
+++ JsonNode.cpp
@@ -1,206 +1,184 @@
 #include "JsonNode.h"
 
-JsonValue::JsonValue():
-	next( nullptr ) , member( nullptr ) 
-{}
+JsonValue::JsonValue() : next(nullptr), member(nullptr) {}
 
-void JsonValue::breakLinks(){
-	next = nullptr;
-	member = nullptr;
+void JsonValue::breakLinks() {
+    next = nullptr;
+    member = nullptr;
 }
 
 JsonValue::~JsonValue() {
-	if( next ){
-		delete next;
-		next = nullptr;
-	}
-	if( member ){
-		delete member;
-		member = nullptr;
-	}
+    if (next) {
+        delete next;
+        next = nullptr;
+    }
+    if (member) {
+        delete member;
+        member = nullptr;
+    }
 }
-std::ostream & JsonValue::print( std::ostream & os ) const {
-	return os << "[JsonValue]";
+std::ostream &JsonValue::print(std::ostream &os) const {
+    return os << "[JsonValue]";
 }
-std::ostream & JsonValue::printJson( std::ostream & os ) const {
+std::ostream &JsonValue::printJson(std::ostream &os) const {
     throw std::runtime_error("printJson this node should be overriden");
 }
-int JsonValue::toLuaObject(LS){
+int JsonValue::toLuaObject(LS) {
     throw std::runtime_error("toLuaObject this node should be overriden");
 }
 
-int JsonValue::asLuaObject(LS){
+int JsonValue::asLuaObject(LS) {
     int old_stack_size = lua_gettop(L);
     this->toLuaObject(L);
     int new_stack_size = lua_gettop(L);
-    assert( old_stack_size + 1 == new_stack_size );
+    assert(old_stack_size + 1 == new_stack_size);
     return 1;
 }
 
-JsonPair::JsonPair(std::string* k):
-	key(k) {}
+JsonPair::JsonPair(std::string *k) : key(k) {}
 
-JsonPair::~JsonPair(){
-	if( key ) {
-		delete key;
-		key = nullptr;
-	}
+JsonPair::~JsonPair() {
+    if (key) {
+        delete key;
+        key = nullptr;
+    }
 }
-std::ostream & JsonPair::print( std::ostream & os ) const {
-	os << "\"" << *key << "\"=>";
-	member->print(os);
-	return os;
-}
-std::ostream & JsonPair::printJson( std::ostream & os ) const {
-	os << "\"" << *key << "\":";
-	member->printJson(os);
+std::ostream &JsonPair::print(std::ostream &os) const {
+    os << "\"" << *key << "\"=>";
+    member->print(os);
     return os;
 }
-int JsonPair::toLuaObject(LS){
+std::ostream &JsonPair::printJson(std::ostream &os) const {
+    os << "\"" << *key << "\":";
+    member->printJson(os);
+    return os;
+}
+int JsonPair::toLuaObject(LS) {
     member->toLuaObject(L);
-    lua_setfield(L, -2, key->c_str() );
+    lua_setfield(L, -2, key->c_str());
     return 1;
 }
 
-
-
-std::ostream & JsonObject::print( std::ostream & os ) const {
-	os << "{JsonObject:";
-	for( JsonValue * ele = member; ele; ele = ele->next ){
-		os << " " ;
-		ele->print(os);
-	}
-	os << "}";
-	return os;
+std::ostream &JsonObject::print(std::ostream &os) const {
+    os << "{JsonObject:";
+    for (JsonValue *ele = member; ele; ele = ele->next) {
+        os << " ";
+        ele->print(os);
+    }
+    os << "}";
+    return os;
 }
-std::ostream & JsonObject::printJson( std::ostream & os ) const {
-	os << "{";
-	for( JsonValue * ele = member; ele; ele = ele->next ){
-		ele->printJson(os);
-        if( ele->next ) os << ",";
-	}
-	os << "}";
-	return os;
+std::ostream &JsonObject::printJson(std::ostream &os) const {
+    os << "{";
+    for (JsonValue *ele = member; ele; ele = ele->next) {
+        ele->printJson(os);
+        if (ele->next)
+            os << ",";
+    }
+    os << "}";
+    return os;
 }
-int JsonObject::toLuaObject(LS){
+int JsonObject::toLuaObject(LS) {
     lua_newtable(L);
-	for( JsonPair* ele = (JsonPair*)member; ele; 
-            ele = (JsonPair*)ele->next )
-    {
-		ele->member->asLuaObject(L);
-        lua_setfield(L, -2, ele->key->c_str() );
+    for (JsonPair *ele = (JsonPair *)member; ele; ele = (JsonPair *)ele->next) {
+        ele->member->asLuaObject(L);
+        lua_setfield(L, -2, ele->key->c_str());
     }
     return 1;
 }
 
-
-
-
-
-
-
-
-std::ostream & JsonArray:: print( std::ostream & os ) const {
-	os << "[JsonArray:";
-	for( JsonValue * ele = member; ele; ele = ele->next ){
-		os << " " ;
-		ele->print(os);
-	}
-	os << "]";
-	return os;
-}std::ostream & JsonArray::printJson( std::ostream & os ) const {
-	os << "[";
-	for( JsonValue * ele = member; ele; ele = ele->next ){
-		ele->printJson(os);
-        if( ele->next ) os << ",";
-	}
-	os << "]";
-	return os;
+std::ostream &JsonArray::print(std::ostream &os) const {
+    os << "[JsonArray:";
+    for (JsonValue *ele = member; ele; ele = ele->next) {
+        os << " ";
+        ele->print(os);
+    }
+    os << "]";
+    return os;
 }
-int JsonArray::toLuaObject(LS){
+std::ostream &JsonArray::printJson(std::ostream &os) const {
+    os << "[";
+    for (JsonValue *ele = member; ele; ele = ele->next) {
+        ele->printJson(os);
+        if (ele->next)
+            os << ",";
+    }
+    os << "]";
+    return os;
+}
+int JsonArray::toLuaObject(LS) {
     lua_newtable(L);
     int idx = 1;
-	for( JsonValue *ele = member; ele; ele = ele->next ){
+    for (JsonValue *ele = member; ele; ele = ele->next) {
         ele->asLuaObject(L);
-        lua_rawseti(L, -2, idx );
+        lua_rawseti(L, -2, idx);
         idx++;
     }
     return 1;
 }
 
-JsonString:: JsonString( std::string *s) : value(s) {
+JsonString::JsonString(std::string *s) : value(s) {}
+JsonString::~JsonString() {
+    if (value) {
+        delete value;
+        value = nullptr;
+    }
 }
-JsonString::~JsonString(){
-	if(value){
-		delete value;
-		value = nullptr;
-	}
+std::ostream &JsonString::print(std::ostream &os) const {
+    return os << "[JsonString: \"" << *value << "\"]";
 }
-std::ostream & JsonString::print( std::ostream & os ) const {
-	return os << "[JsonString: \"" << *value << "\"]";
+std::ostream &JsonString::printJson(std::ostream &os) const {
+    return os << "\"" << *value << "\"";
 }
-std::ostream & JsonString::printJson( std::ostream & os ) const {
-	return os << "\"" << *value << "\"";
-}
-int JsonString::toLuaObject(LS)
-{
-    lua_pushlstring(L, value->data(), value->length() );
+int JsonString::toLuaObject(LS) {
+    lua_pushlstring(L, value->data(), value->length());
     return 1;
 }
 
-
-
 JsonNumber::JsonNumber(double v) : value(v) {}
-std::ostream & JsonNumber::print( std::ostream & os ) const {
-	return os << "[JsonNumber: " << value << "]";
+std::ostream &JsonNumber::print(std::ostream &os) const {
+    return os << "[JsonNumber: " << value << "]";
 }
-std::ostream & JsonNumber::printJson( std::ostream & os ) const {
-	return os << value;
+std::ostream &JsonNumber::printJson(std::ostream &os) const {
+    return os << value;
 }
-int JsonNumber::toLuaObject(LS)
-{
+int JsonNumber::toLuaObject(LS) {
     lua_pushnumber(L, value);
     return 1;
 }
 
-
-
-
-
-JsonBoolean::JsonBoolean(bool b): value(b) {}
-std::ostream & JsonBoolean:: print( std::ostream & os ) const {
-	return os << "[JsonBoolean: " << (value?"true":"false") << "]";
+JsonBoolean::JsonBoolean(bool b) : value(b) {}
+std::ostream &JsonBoolean::print(std::ostream &os) const {
+    return os << "[JsonBoolean: " << (value ? "true" : "false") << "]";
 }
-std::ostream & JsonBoolean:: printJson( std::ostream & os ) const {
-	return os << (value?"true":"false");
+std::ostream &JsonBoolean::printJson(std::ostream &os) const {
+    return os << (value ? "true" : "false");
 }
-int JsonBoolean::toLuaObject(LS){
+int JsonBoolean::toLuaObject(LS) {
     lua_pushboolean(L, value);
     return 1;
 }
 
-
-std::ostream & JsonNull::print( std::ostream & os ) const {
-	return os << "[JsonNull]";
+std::ostream &JsonNull::print(std::ostream &os) const {
+    return os << "[JsonNull]";
 }
-std::ostream & JsonNull::printJson( std::ostream & os ) const {
-	return os << "null";
+std::ostream &JsonNull::printJson(std::ostream &os) const {
+    return os << "null";
 }
-int JsonNull::toLuaObject(LS){
+int JsonNull::toLuaObject(LS) {
     lua_pushnil(L);
     return 1;
 }
 
+JsonState::JsonState() : value(nullptr) {}
 
-JsonState::JsonState() : value(nullptr){ }
+JsonValue *JsonState::getJsonValue() const { return value; }
 
-JsonValue * JsonState::getJsonValue() const { return value; }
-
-void JsonState::free(){
-	for( auto ptr : objList ){
-		ptr->breakLinks();
-		delete ptr;
-	}
-	objList.clear();
-	value = nullptr;
+void JsonState::free() {
+    for (auto ptr : objList) {
+        ptr->breakLinks();
+        delete ptr;
+    }
+    objList.clear();
+    value = nullptr;
 }
--- json2tds.cpp
+++ json2tds.cpp
@@ -1,5 +1,5 @@
 #if defined LS
-    #undef LS
+#undef LS
 #endif
 #define LS lua_State *L
 
@@ -7,61 +7,54 @@
 #include "JsonNode.h"
 
 extern "C" {
-    #include <lua.h>
-    #include <lualib.h>
-    #include <lauxlib.h>
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
 
-    int luaopen_json2tds(LS);
-    int json_decode(LS);
+int luaopen_json2tds(LS);
+int json_decode(LS);
 }
 
-struct OnExitScope{
-    std::function< void() > func;
-    template<class Func>
-    OnExitScope( Func f ): func(f) {}
-    ~OnExitScope(){
-        func();
-    }
+struct OnExitScope {
+    std::function<void()> func;
+    template <class Func> OnExitScope(Func f) : func(f) {}
+    ~OnExitScope() { func(); }
 };
 
-#define TOKENPASTE(x, y) x ## y
+#define TOKENPASTE(x, y) x##y
 #define TOKENPASTE2(x, y) TOKENPASTE(x, y)
-#define onexit(x) OnExitScope TOKENPASTE2(__oes, __LINE__) \
-    { [&]{x;} }
+#define onexit(x)                                                              \
+    OnExitScope TOKENPASTE2(__oes, __LINE__) {                                 \
+        [&] { x; }                                                             \
+    }
 
-int json_decode(LS)
-{
-    const char * filepath = luaL_checkstring(L, 1);
+int json_decode(LS) {
+    const char *filepath = luaL_checkstring(L, 1);
 
     FILE *fp = NULL;
-    JsonValue * json = nullptr;
+    JsonValue *json = nullptr;
 
-    onexit( 
-        if( fp ) fclose(fp);
-        if( json ) delete json;
-    );
-    
+    onexit(if (fp) fclose(fp); if (json) delete json;);
+
     fp = fopen(filepath, "rb");
 
-    if( fp == NULL ){
+    if (fp == NULL) {
         lua_pushnil(L);
         return 1;
     }
 
     json = parse_json(fp);
 
-    if( json != NULL ){
+    if (json != NULL) {
         json->toLuaObject(L);
-    }else{
+    } else {
         lua_pushnil(L);
     }
 
     return 1;
 }
 
-
-int luaopen_json2tds(lua_State *L)
-{
+int luaopen_json2tds(lua_State *L) {
 
     lua_newtable(L);
     lua_pushcfunction(L, json_decode);
